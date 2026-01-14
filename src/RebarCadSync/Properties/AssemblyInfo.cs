007b29a68b679031db8a404bac7101c9a8ec7443
gzhqwe3-netizen <gzhqwe3@gmail.com>
2026-01-14 17:19:53 +0000

    Add RebarCadSync AutoCAD plugin project

---
+++ src/RebarCadSync/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RebarCadSync")]
+[assembly: AssemblyDescription("AutoCAD plugin to sync rebar annotations from Excel.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RebarCadSync")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
